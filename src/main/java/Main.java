595a7ee3cf9ee736a38784395252c73437840a34
Sonu <notifications3972@gmail.com>
2017-10-17 17:29:18 -0700

    Reading From Firebase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,70 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import com.google.firebase.database.*;
 import org.apache.log4j.BasicConfigurator;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import org.json.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
     	BasicConfigurator.configure();
+		setupFirebase();
+		final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         get("/hello", (req, res) -> "Hello World");
 
-        //Setting up Firebase
-        setupFirebase();
+		get("/addQuestion", (request, response) ->{
+			//System.out.println("UUID: " + request.queryParams("uuid"));
+			//System.out.println("Creating Wallet");
+			DatabaseReference ref = database.getReference("Question/TF");
+			ref.push().setValue("Question");
 
-		final FirebaseDatabase database = FirebaseDatabase.getInstance();
-		DatabaseReference ref = database.getReference("server/saving-data/fireblog");
+			return true;
+		});
 
-		ref.setValue("test4");
+		get("/getQuestion", (request, response) ->{
+			//System.out.println("UUID: " + request.queryParams("uuid"));
+			//System.out.println("Creating Wallet");
+			DatabaseReference ref = database.getReference("Question/TF");
+
+			// Attach a listener to read the data at our posts reference
+			ref.addValueEventListener(new ValueEventListener() {
+				@Override
+				public void onDataChange(DataSnapshot dataSnapshot) {
+					System.out.println(dataSnapshot.getValue());
+					//JSONObject temp = dataSnapshot.getValue();
+				}
+
+				@Override
+				public void onCancelled(DatabaseError databaseError) {
+					System.out.println("The read failed: " + databaseError.getCode());
+				}
+			});
+
+			return true;
+		});
+
+		DatabaseReference ref = database.getReference("Question/TF");
+		
+		System.out.println("Before Readng");
+
+		// Attach a listener to read the data at our posts reference
+		ref.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				System.out.println("Value: " + dataSnapshot.getValue());
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
+				System.out.println("The read failed: " + databaseError.getCode());
+			}
+		});
+
     }
 
     public static void setupFirebase(){
